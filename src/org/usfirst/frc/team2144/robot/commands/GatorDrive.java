2b839b14875ea55583ce20cfcfcad4d0394cf8cc
Andrew Zhu <mommothazaz123@gmail.com>
2017-01-21 16:07:41 -0800

    added end() call in gatorDrive

--- src/org/usfirst/frc/team2144/robot/commands/GatorDrive.java
+++ src/org/usfirst/frc/team2144/robot/commands/GatorDrive.java
@@ -33,5 +33,6 @@ public class GatorDrive extends CommandBase {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
