92d23781bdf6da9f38691d62ec440a08288ad646
Andrew Zhu <mommothazaz123@gmail.com>
2017-01-24 10:09:20 -0800

    began tankanum

--- src/org/usfirst/frc/team2144/robot/commands/GatorDrive.java
+++ src/org/usfirst/frc/team2144/robot/commands/GatorDrive.java
@@ -1,38 +1,40 @@
 package org.usfirst.frc.team2144.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
-
 /**
  *
  */
 public class GatorDrive extends CommandBase {
 
-    public GatorDrive() {
-        // Use requires() here to declare subsystem dependencies
-        requires(drivetrain);
-    }
+	public GatorDrive() {
+		// Use requires() here to declare subsystem dependencies
+		requires(drivetrain);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	drivetrain.mecanum_cartesian(oi.getStickX(), oi.getStickY(), oi.getStickRot(), 0);
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		if (oi.getStick2Mecanum() != 0) {
+			drivetrain.mecanum_cartesian(oi.getStick2Mecanum(), oi.getStick2Y(), oi.getStick2Rot(), 0);
+		} else {
+			drivetrain.tank(oi.getStickY(), oi.getStick2Y());
+		}
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	end();
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
 }
