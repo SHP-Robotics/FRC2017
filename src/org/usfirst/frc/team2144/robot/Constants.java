3f754b0c75bb059cea2c305902b9509a7525a3c8
Andrew Zhu <mommothazaz123@gmail.com>
2017-02-13 12:33:02 -0800

    finished gearmanip

--- src/org/usfirst/frc/team2144/robot/Constants.java
+++ src/org/usfirst/frc/team2144/robot/Constants.java
@@ -6,15 +6,15 @@ public class Constants {
 	public static double K_VISION_TOLERANCE = 3; // in px
 	
 	// PID
-	public static double K_GEAR_P = 1;
-	public static double K_GEAR_I = 0.1;
-	public static double K_GEAR_D = 0.1;
+	public static double K_GEAR_P = 15;
+	public static double K_GEAR_I = 0;
+	public static double K_GEAR_D = 0;
 	
 	// Dynamic constants
 	public static int D_SHOOTER_FIRE = 25;
 	public static int D_SHOOTER_REST = 0;
 	public static double D_SHOOTER_INTAKE_PWR = 0.5;
 	
-	public static double D_GEAR_OPEN = 0.5;
-	public static double D_GEAR_CLOSE = 0.4;
+	public static double D_GEAR_OPEN = 0.2377;
+	public static double D_GEAR_CLOSE = 0.1154;
 }
--- src/org/usfirst/frc/team2144/robot/subsystems/GearManip.java
+++ src/org/usfirst/frc/team2144/robot/subsystems/GearManip.java
@@ -2,9 +2,10 @@ package org.usfirst.frc.team2144.robot.subsystems;
 
 import org.usfirst.frc.team2144.robot.Constants;
 import org.usfirst.frc.team2144.robot.RobotMap;
+import org.usfirst.frc.team2144.robot.commands.GearManipDrive;
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
 /**
@@ -12,12 +13,12 @@ import edu.wpi.first.wpilibj.command.PIDSubsystem;
  */
 public class GearManip extends PIDSubsystem {
 	
-	private Talon motor;
+	private Spark motor;
 	private AnalogPotentiometer pot;
 	
 	public GearManip() {
 		super("GearManip", Constants.K_GEAR_P, Constants.K_GEAR_I, Constants.K_GEAR_D);
-		motor = new Talon(RobotMap.GEAR_MOTOR);
+		motor = new Spark(RobotMap.GEAR_MOTOR);
 		pot = new AnalogPotentiometer(RobotMap.GEAR_POT);
 		setAbsoluteTolerance(0.005);
 	}
@@ -25,7 +26,7 @@ public class GearManip extends PIDSubsystem {
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
-        
+        setDefaultCommand(new GearManipDrive());
     }
 
 	protected double returnPIDInput() {
